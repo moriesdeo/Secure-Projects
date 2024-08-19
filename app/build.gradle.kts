bf8c7d504d9ca3b68f83ee2bcc13b870b16a9a8e
MORIES DEO HUTAPEA <moriesdeo242@gmail.com>
2024-08-19 13:18:29 +0700

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "id.secure.prefs"
         minSdk = 23
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
